c144520c540dd580a1038f17ff85e2361886f6e2
neoarz <tyrantneo740@gmail.com>
2026-02-08 16:36:35 -0500

    fix: vite config revert when we add back domain

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/willyoupleasefuck.me/',
   plugins: [svelte()],
   build: {
     minify: 'terser',
