7599bd3fc8bceb4edd253f512a536f4d6179e5f8
neoarz <tyrantneo740@gmail.com>
2026-02-08 16:47:31 -0500

    revert: base path for domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/willyoupleasefuck.me/',
   plugins: [svelte()],
   build: {
     minify: 'terser',
